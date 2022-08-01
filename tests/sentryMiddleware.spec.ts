378da80fff46e97dc015da4b1de14c24cd00913c
Thanaen <Thanaen@users.noreply.github.com>
2022-08-01 21:52:23 +0200

    test: add sentryMiddleware test

---
+++ tests/sentryMiddleware.spec.ts
@@ -0,0 +1,39 @@
+import create from "zustand/vanilla";
+import sentryMiddleware from "../src";
+
+// Mock Sentry's configureScope method.
+const setContextMock = jest.fn();
+const scopeMock = {
+  setContext: setContextMock,
+};
+const Sentry = {
+  configureScope: (callback: any) => callback(scopeMock),
+};
+
+interface BearState {
+  bears: number;
+  increase: (by: number) => void;
+}
+
+const store = create<BearState>()(
+  sentryMiddleware(
+    (set) => ({
+      bears: 0,
+      increase: (by) => set((state) => ({ bears: state.bears + by })),
+    }),
+    { sentry: Sentry as any }
+  )
+);
+
+beforeEach(() => {
+  setContextMock.mockClear();
+});
+
+test("sentryMiddleware", () => {
+  const { increase } = store.getState();
+  increase(1);
+  expect(setContextMock).toHaveBeenCalledWith("state", {
+    type: "zustand",
+    value: { bears: 1, increase: expect.any(Function) },
+  });
+});
